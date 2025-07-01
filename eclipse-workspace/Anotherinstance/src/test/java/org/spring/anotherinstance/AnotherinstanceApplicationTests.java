bd6fe8b618f1b5235c9212ff56cf28c2f6c71691
HAMZA NASSIF <abn.nassif@gmail.com>
2025-07-01 17:24:06 +0100

    newchanges

---
+++ eclipse-workspace/Anotherinstance/src/test/java/org/spring/anotherinstance/AnotherinstanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.anotherinstance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherinstanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
