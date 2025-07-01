2e946378bd86220254a2e2a9ad599882d3156864
HAMZA NASSIF <abn.nassif@gmail.com>
2025-07-01 17:47:57 +0100

    news

---
+++ src/main/java/org/spring/anotherinstance/Model/userDto.java
@@ -0,0 +1,31 @@
+package org.spring.anotherinstance.Model;
+
+public class userDto {
+  private User user;
+private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public userDto() {
+    }
+
+    public userDto(User user,String token) {
+
+this.user=user;
+this.token=token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
